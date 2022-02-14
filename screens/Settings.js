8fd8a78ef587fec1b92e87a7629c18c6e6e74299
lolguy66 <alexanderflotti@gmail.com>
2022-02-14 12:20:13 +0000

    oops

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,10 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
 
-<<<<<<< HEAD
-export default function Profile() {
-=======
 export default function SettingsScreen() {
->>>>>>> 4db9baa215185f4fbf72b82b1d5661344eec76bf
     return (
         <View style={styles.container}>
             <Text>TODO: Add some settings here</Text>
@@ -14,8 +10,4 @@ export default function SettingsScreen() {
 
 const styles = StyleSheet.create({
     container: {}
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> 4db9baa215185f4fbf72b82b1d5661344eec76bf
