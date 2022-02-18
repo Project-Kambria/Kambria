370d4b66d8aa2d41f2c49e8c7eb3056721519dea
Tristan Andersen <tristanpn02@gmail.com>
2022-02-18 08:54:22 +0000

    Create Fridge.js

---
+++ screens/Fridge.js
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function FridgeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>TODO: Add some fridge stuff here</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {}
+})
